f0a69d02808d977e8dee458747e6ad21f2045ed8
fox-dda <fox-dda@yandex.ru>
2019-10-29 16:45:28 +0700

    Создан проект

---
+++ SiamCross/SiamCross.Android/MainActivity.cs
@@ -0,0 +1,59 @@
+﻿using System;
+
+using Android.App;
+using Android.Content.PM;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+using Android.OS;
+using Android;
+using Android.Support.V4.Content;
+using Android.Support.V4.App;
+
+namespace SiamCross.Droid
+{
+    [Activity(Label = "SiamCross", Icon = "@mipmap/icon", Theme = "@style/MainTheme", MainLauncher = true, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation)]
+    public class MainActivity : global::Xamarin.Forms.Platform.Android.FormsAppCompatActivity
+    {
+        protected override void OnCreate(Bundle savedInstanceState)
+        {
+            TabLayoutResource = Resource.Layout.Tabbar;
+            ToolbarResource = Resource.Layout.Toolbar;
+
+            base.OnCreate(savedInstanceState);
+
+            Xamarin.Essentials.Platform.Init(this, savedInstanceState);
+            global::Xamarin.Forms.Forms.Init(this, savedInstanceState);
+
+            var locationPermissions = new[]
+{
+                Manifest.Permission.AccessCoarseLocation,
+                Manifest.Permission.AccessFineLocation
+            };
+
+            // check if the app has permission to access coarse location
+            var coarseLocationPermissionGranted =
+                ContextCompat.CheckSelfPermission(this, Manifest.Permission.AccessCoarseLocation);
+
+            // check if the app has permission to access fine location
+            var fineLocationPermissionGranted =
+                ContextCompat.CheckSelfPermission(this, Manifest.Permission.AccessFineLocation);
+
+            // if either is denied permission, request permission from the user
+            const int locationPermissionsRequestCode = 1000;
+            if (coarseLocationPermissionGranted == Permission.Denied ||
+                fineLocationPermissionGranted == Permission.Denied)
+            {
+                ActivityCompat.RequestPermissions(this, locationPermissions, locationPermissionsRequestCode);
+            }
+
+            LoadApplication(new App());
+        }
+        public override void OnRequestPermissionsResult(int requestCode, string[] permissions, [GeneratedEnum] Android.Content.PM.Permission[] grantResults)
+        {
+            Xamarin.Essentials.Platform.OnRequestPermissionsResult(requestCode, permissions, grantResults);
+
+            base.OnRequestPermissionsResult(requestCode, permissions, grantResults);
+        }
+    }
+}
---
+++ SiamCross/SiamCross/Models/IBluetoothScaner.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SiamCross.Models
+{
+    public interface IBluetoothScaner
+    {
+        void Start();
+        void Stop();
+        event Action<string, object> Received;
+    }
+}
