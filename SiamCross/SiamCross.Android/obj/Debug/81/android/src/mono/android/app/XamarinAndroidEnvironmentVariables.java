4423e78bcf85d606f96f553f41cc56dd6036dc10
fox-dda <fox-dda@yandex.ru>
2019-10-30 15:45:45 +0700

    Создание "hamburger" menu

--- SiamCross/SiamCross.Android/obj/Debug/81/android/src/mono/android/app/XamarinAndroidEnvironmentVariables.java
+++ SiamCross/SiamCross.Android/obj/Debug/81/android/src/mono/android/app/XamarinAndroidEnvironmentVariables.java
@@ -5,7 +5,7 @@ public class XamarinAndroidEnvironmentVariables
 	// Variables are specified the in "name", "value" pairs
 	public static final String[] Variables = new String[] {
 		"MONO_LOG_LEVEL", "info",
-		"XAMARIN_BUILD_ID", "d18cd437-6bff-456c-81e7-bdcc9c425487",
+		"XAMARIN_BUILD_ID", "7c22494f-719d-43fc-ae28-dbe5c9e1240c",
 		"XA_HTTP_CLIENT_HANDLER_TYPE", "Xamarin.Android.Net.AndroidClientHandler",
 		"XA_TLS_PROVIDER", "btls",
 		"MONO_GC_PARAMS", "major=marksweep-conc",
