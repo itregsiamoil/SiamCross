56a023f405477ac211fb400d5de4637d4b164bd3
fox-dda <fox-dda@yandex.ru>
2019-11-06 11:22:41 +0700

    получены изменения с мастера

--- SiamCross/SiamCross.Android/obj/Debug/81/android/src/mono/android/app/XamarinAndroidEnvironmentVariables.java
+++ SiamCross/SiamCross.Android/obj/Debug/81/android/src/mono/android/app/XamarinAndroidEnvironmentVariables.java
@@ -5,7 +5,7 @@ public class XamarinAndroidEnvironmentVariables
 	// Variables are specified the in "name", "value" pairs
 	public static final String[] Variables = new String[] {
 		"MONO_LOG_LEVEL", "info",
-		"XAMARIN_BUILD_ID", "7c22494f-719d-43fc-ae28-dbe5c9e1240c",
+		"XAMARIN_BUILD_ID", "771c4ad1-acfb-445b-bfb1-12e3288706df",
 		"XA_HTTP_CLIENT_HANDLER_TYPE", "Xamarin.Android.Net.AndroidClientHandler",
 		"XA_TLS_PROVIDER", "btls",
 		"MONO_GC_PARAMS", "major=marksweep-conc",
