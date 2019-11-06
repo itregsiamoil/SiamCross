c753a8ebacb23da460e16558cbd0ff34076e1a3b
Гелцен Савындай <5842_sgy@keva.int>
2019-11-06 12:07:13 +0700

    add new solution without trash

---
+++ SiamCross/SiamCross/App.xaml.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+using SiamCross.Views;
+
+namespace SiamCross
+{
+    public partial class App : Application
+    {
+        public static NavigationPage NavigationPage { get; private set; }
+        public static RootPage RootPage;
+        public static bool MenuIsPresented
+        {
+            get
+            {
+                return RootPage.IsPresented;
+            }
+            set
+            {
+                RootPage.IsPresented = value;
+            }
+        }
+
+        private void CallMain()
+        {
+            var menuPage = new MenuPage();
+            NavigationPage = new NavigationPage(new Home());
+            RootPage = new RootPage();
+            RootPage.Master = menuPage;
+            RootPage.Detail = NavigationPage;
+            MainPage = RootPage;
+        }
+
+        public App()
+        {
+            InitializeComponent();
+
+            // MainPage = new MainPage();
+            CallMain();
+        }
+
+        protected override void OnStart()
+        {
+            // Handle when your app starts
+        }
+
+        protected override void OnSleep()
+        {
+            // Handle when your app sleeps
+        }
+
+        protected override void OnResume()
+        {
+            // Handle when your app resumes
+        }
+    }
+}
---
+++ SiamCross/SiamCross/Models/MainPageModel.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SiamCross.Models
+{
+    public class MainPageModel
+    {
+        public Dictionary<string, object> DeviceDict = new Dictionary<string, object>();
+    }
+}
