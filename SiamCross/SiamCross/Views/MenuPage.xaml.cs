9952d819b6606ff9b55b4d511b4622a9ab79ec73
fox-dda <fox-dda@yandex.ru>
2019-11-06 10:42:06 +0700

    Добавление каркаса

---
+++ SiamCross/SiamCross/Views/MenuPage.xaml.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using SiamCross.ViewModels;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace SiamCross.Views
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class MenuPage : ContentPage
+    {
+        public MenuPage()
+        {
+            BindingContext = new MenuPageViewModel();
+            //this.Icon = "yourHamburgerIcon.png"; //only neeeded for ios
+            InitializeComponent();
+        }
+    }
+}
