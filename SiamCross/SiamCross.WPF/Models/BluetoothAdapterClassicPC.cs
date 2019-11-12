7396b576e8366472459fe31abdf8401194b2e969
Гелцен Савындай <5842_sgy@keva.int>
2019-11-12 14:29:47 +0700

    Добавлен класс BluetoothAdapterClassicPC для второй версии блютуза

---
+++ SiamCross/SiamCross.WPF/Models/BluetoothAdapterClassicPC.cs
@@ -0,0 +1,31 @@
+﻿using SiamCross.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SiamCross.WPF.Models
+{
+    public class BluetoothAdapterClassicPC : IBluetoothAdapter
+    {
+
+
+        public event Action<byte[]> DataReceived;
+
+        public Task Connect(object connectArgs)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task Disconnect()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task SendData(byte[] data)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
