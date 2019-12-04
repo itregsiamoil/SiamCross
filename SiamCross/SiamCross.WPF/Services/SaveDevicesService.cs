a0c15e5b6a6035d2e78fb2fa23a63a7674bc5476
Гелцен Савындай <5842_sgy@keva.int>
2019-12-04 12:02:11 +0700

    Добавлен сервис для сохранения и загрузки устройств из панели управления

---
+++ SiamCross/SiamCross.WPF/Services/SaveDevicesService.cs
@@ -0,0 +1,23 @@
+﻿using SiamCross.Models.Scanners;
+using SiamCross.Services;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SiamCross.WPF.Services
+{
+    public class SaveDevicesService : ISaveDevicesService
+    {
+        public Task<IEnumerable<ScannedDeviceInfo>> LoadDevices()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task SaveDevices(IEnumerable<ScannedDeviceInfo> devices)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ SiamCross/SiamCross/Services/ISaveDevicesService.cs
@@ -0,0 +1,15 @@
+﻿using SiamCross.Models.Scanners;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SiamCross.Services
+{
+    public interface ISaveDevicesService
+    {
+        Task SaveDevices(IEnumerable<ScannedDeviceInfo> devices);
+
+        Task<IEnumerable<ScannedDeviceInfo>> LoadDevices();
+    }
+}
