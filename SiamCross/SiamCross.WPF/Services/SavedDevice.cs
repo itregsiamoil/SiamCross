d2f89cd12eb1b32311135c93da0350a2cf914d82
Гелцен Савындай <5842_sgy@keva.int>
2019-12-04 12:09:52 +0700

    Добавлен класс SavedDevice для сериализации устройства

---
+++ SiamCross/SiamCross.WPF/Services/SavedDevice.cs
@@ -0,0 +1,12 @@
+﻿using Newtonsoft.Json;
+
+namespace SiamCross.WPF.Services
+{
+    [JsonObject(MemberSerialization.Fields)]
+    public class SavedDevice
+    {
+        public string DeviceName;
+
+        public string DeviceAddress;
+    }
+}
