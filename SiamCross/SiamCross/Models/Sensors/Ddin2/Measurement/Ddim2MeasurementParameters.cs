1d41633c9a533d18bd43958234574069980ad5f7
fox-dda <fox-dda@yandex.ru>
2019-12-11 11:51:30 +0700

    Добавлены структуры данных для замера ддим2

---
+++ SiamCross/SiamCross/Models/Sensors/Ddin2/Measurement/Ddim2MeasurementParameters.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SiamCross.Models.Sensors.Ddin2.Measurement
+{
+    public struct Ddim2MeasurementParameters
+    {
+        public int Rod { get; }
+        public int DynPeriod { get; }
+        public int ApertNumber { get; }
+        public int Imtravel { get; }
+        public int ModelPump { get; }
+
+        public Ddim2MeasurementParameters(int rod,
+                                 int dynPeriod,
+                                 int apertNumber,
+                                 float imtravel,
+                                 int modelPump)
+        {
+            Rod = rod * 10;
+            DynPeriod = dynPeriod * 1000;
+            ApertNumber = apertNumber;
+            Imtravel = Convert.ToInt32(imtravel * 1000);
+            ModelPump = modelPump;
+        }
+    }
+}
