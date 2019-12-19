10f638b9f93244db427c5834f960e0ccdf73b250
Гелцен Савындай <5842_sgy@keva.int>
2019-12-19 13:28:05 +0700

    Добавлена папка для валидаторов

---
+++ SiamCross/SiamCross/Views/Ddim2MeasurementDonePage.xaml.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace SiamCross.Views
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class Ddim2MeasurementDonePage : ContentPage
+    {
+        public Ddim2MeasurementDonePage()
+        {
+            InitializeComponent();
+        }
+    }
+}
