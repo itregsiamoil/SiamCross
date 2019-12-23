31ec73039c33333a7f4e78d750f0774612b81bcf
Гелцен Савындай <5842_sgy@keva.int>
2019-12-23 16:16:02 +0700

    Добавлен класс вьюмодели для страницы после измерения ДДин2

---
+++ SiamCross/SiamCross/ViewModels/Ddin2MeasurementDoneViewModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SiamCross.ViewModels
+{
+    public class Ddin2MeasurementDoneViewModel : BaseViewModel, IViewModel
+    {
+        public Ddin2MeasurementDoneViewModel()
+        {
+
+        }
+    }
+}
