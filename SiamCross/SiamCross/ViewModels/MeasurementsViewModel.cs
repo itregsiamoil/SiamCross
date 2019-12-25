bc9941c3c8fb20f6c3261f0448d86cefad4046aa
Гелцен Савындай <5842_sgy@keva.int>
2019-12-25 15:03:33 +0700

    Добавлена вьюмоделька для страницы измерений

---
+++ SiamCross/SiamCross/ViewModels/MeasurementsViewModel.cs
@@ -0,0 +1,51 @@
+﻿using SiamCross.Services;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+
+namespace SiamCross.ViewModels
+{
+    public class MeasurementsViewModel : BaseViewModel, IViewModel
+    {
+        public class MeasurementView
+        {
+            public string Field { get; set; }
+            public DateTime Date { get; set; }
+            public string MeasurementType { get; set; }
+            public string Comments { get; set; }
+        }
+
+        public ObservableCollection<MeasurementView> Measurements { get; set; }
+
+        public MeasurementsViewModel()
+        {
+            var ddim2Measurements = DataRepository.Instance.GetDdim2Items();
+            var ddin2Measurements = DataRepository.Instance.GetDdin2Items();
+            foreach (var m in ddim2Measurements)
+            {
+                Measurements.Add(
+                    new MeasurementView
+                    {
+                        Field = m.Field,
+                        Date = m.DateTime,
+                        MeasurementType = "Динамограмма",
+                        Comments = m.Comment
+                    });
+            }
+            foreach (var m in ddin2Measurements)
+            {
+                Measurements.Add(
+                    new MeasurementView
+                    {
+                        Field = m.Field,
+                        Date = m.DateTime,
+                        MeasurementType = "Динамограмма",
+                        Comments = m.Comment
+                    });
+            }
+            Measurements.OrderBy(m => m.Date);
+        }
+    }
+}
