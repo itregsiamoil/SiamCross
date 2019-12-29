9d15fa78ab87d898fc1a42516253eceb9f7dfaf4
fox-dda <fox-dda@yandex.ru>
2019-12-29 11:46:26 +0700

    Добавлена калассы для работы с файлами на разных платформах

--- SiamCross/SiamCross.WPF/Models/SQLiteWPF.cs
+++ SiamCross/SiamCross.WPF/Models/SQLiteWPF.cs
@@ -12,7 +12,6 @@ namespace SiamCross.WPF.Models
         public SQLiteWPF() { }
         public string GetDatabasePath(string sqliteFilename)
         {
-            // для доступа к файлам используем API Windows.Storage
             string path = Path.Combine(Directory.GetCurrentDirectory(), sqliteFilename);
             return path;
         }
