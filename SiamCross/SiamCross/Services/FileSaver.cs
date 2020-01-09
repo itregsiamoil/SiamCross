cd584324f2e528779d7c2d9710a94f891849b86c
fox-dda <fox-dda@yandex.ru>
2020-01-09 09:29:38 +0700

    Добавлен в индекс класс сохранения файлов

---
+++ SiamCross/SiamCross/Services/FileSaver.cs
@@ -0,0 +1,24 @@
+﻿using SiamCross.Models.Tools;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Linq;
+
+namespace SiamCross.Services
+{
+    public class FileSaver
+    {
+        private IFileManager _fileManager;
+
+        public FileSaver(IFileManager fileManager)
+        {
+            _fileManager = fileManager;
+        }
+
+        public void SaveXml(string filename, XDocument xml)
+        {
+            xml.Save(filename);
+        }
+    }
+}
