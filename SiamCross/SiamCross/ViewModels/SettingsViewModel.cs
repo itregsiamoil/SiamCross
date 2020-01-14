468909713f9277135f37dab541703fe8ae06dedd
Гелцен Савындай <5842_sgy@keva.int>
2020-01-14 11:55:10 +0700

    Добавлен SettingsViewModel

---
+++ SiamCross/SiamCross/ViewModels/SettingsViewModel.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SiamCross.ViewModels
+{
+    public class SettingsViewModel : BaseViewModel, IViewModel
+    {
+        public string FromAddress { get; set; }
+        public string ToAddress { get; set; }
+        public string SmtpAddress { get; set; }
+        public string Port { get; set; }
+        public string Username { get; set; }
+        public string Password { get; set; }
+
+        public SettingsViewModel()
+        {
+
+        }
+    }
+}
--- SiamCross/SiamCross/Views/MenuItems/SettingsPanelPage.xaml.cs
+++ SiamCross/SiamCross/Views/MenuItems/SettingsPanelPage.xaml.cs
@@ -15,27 +15,9 @@ namespace SiamCross.Views.MenuItems
     [XamlCompilation(XamlCompilationOptions.Compile)]
     public partial class SettingsPanelPage : ContentPage
     {
-
         public SettingsPanelPage()
         {
             InitializeComponent();
-        }
-
-        private void CanvasView_PaintSurface(object sender, SkiaSharp.Views.Forms.SKPaintSurfaceEventArgs args)
-        {
-            SKImageInfo info = args.Info;
-            SKSurface surface = args.Surface;
-            SKCanvas canvas = surface.Canvas;
-
-            canvas.Clear();
-
-            SKPaint paint = new SKPaint
-            {
-                Style = SKPaintStyle.Stroke,
-                Color = Color.Red.ToSKColor(),
-                StrokeWidth = 25
-            };
-            canvas.DrawCircle(info.Width / 2, info.Height / 2, 100, paint);
         }
     }
 }
