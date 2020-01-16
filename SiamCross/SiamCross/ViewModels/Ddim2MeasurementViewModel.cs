940733154550c11fe964678223c7ea24a6b46861
Гелцен Савындай <5842_sgy@keva.int>
2020-01-16 09:35:24 +0700

    Исправлен баг с проверкой вхождения числа в промежуток

--- SiamCross/SiamCross/ViewModels/Ddim2MeasurementViewModel.cs
+++ SiamCross/SiamCross/ViewModels/Ddim2MeasurementViewModel.cs
@@ -166,7 +166,7 @@ namespace SiamCross.ViewModels
 
         private bool IsNumberValid(int from, int to, int number)
         {
-            return Enumerable.Range(from, to).Contains(number);
+            return number >= from && number <= to;
         }
 
         private bool ValidateForEmptiness()
