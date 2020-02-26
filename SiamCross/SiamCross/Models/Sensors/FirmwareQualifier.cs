04faa54accd5417062c68df6c54fde44c82fc58b
fox-dda <fox-dda@yandex.ru>
2020-02-26 14:23:37 +0700

    Проиндексированы недостающие файлы

---
+++ SiamCross/SiamCross/Models/Sensors/FirmwareQualifier.cs
@@ -0,0 +1,100 @@
+﻿using SiamCross.Models.Sensors.Dynamographs.Shared;
+using SiamCross.Models.Tools;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SiamCross.Models.Sensors
+{
+
+    /// <summary>
+    /// Определитель версии прошивки
+    /// </summary>
+    public class FirmWaveQualifier
+    {
+
+        private Func<byte[], Task> QueryParamerter;
+
+        /// <summary>
+        /// Адресс версии программы
+        /// </summary>
+        private byte[] _programmVersionAddress;
+
+        /// <summary>
+        /// Адресс версии программы
+        /// </summary>
+        public byte[] DeviceNameAddress
+        {
+            get => _programmVersionAddress;
+            set
+            {
+                _programmVersionAddress = value;
+                if (_programmVersionSize != null)
+                {
+                    IsFullFirmWaveInformation(_programmVersionAddress, _programmVersionSize);
+                }
+            }
+        }
+
+        private async Task GetFirmware(byte[] address, byte[] size)
+        {
+            var command = new MessageCreator().CreateReadMessage(address, size);
+            await QueryParamerter(command);
+        }
+
+        public async Task Qualify()
+        {
+            if (DeviceNameAddress == null)
+            {
+                await QueryParamerter(DynamographCommands.FullCommandDictionary["ProgrammVersionAddress"]);
+            }
+            if (DeviceNameSize == null)
+            {
+                await QueryParamerter(DynamographCommands.FullCommandDictionary["ProgrammVersionSize"]);
+            }
+            if(DeviceNameAddress != null && DeviceNameSize != null)
+            {
+                await GetFirmware(DeviceNameAddress, DeviceNameSize);
+            }
+        }
+
+        /// <summary>
+        /// Размер версии программы
+        /// </summary>
+        private byte[] _programmVersionSize;
+
+        public FirmWaveQualifier(Func<byte[], Task> queryParamerters)
+        {
+            QueryParamerter = queryParamerters;
+            IsFullFirmWaveInformation += GetFirmware;
+        }
+
+        /// <summary>
+        /// Размер версии программы
+        /// </summary>
+        public byte[] DeviceNameSize
+        {
+            get
+            {
+                return _programmVersionSize;
+            }
+            set
+            {
+                _programmVersionSize = value;
+                if (_programmVersionAddress != null)
+                {
+                    IsFullFirmWaveInformation(_programmVersionAddress, _programmVersionSize);
+                }
+            }
+        }
+
+        // Делагат полноты данных
+        public delegate Task FirmWaveHandler(byte[] deviceNameAddress, byte[] deviceNameSize);
+
+        /// <summary>
+        /// Событие готовности всех данных
+        /// </summary>
+        public event FirmWaveHandler IsFullFirmWaveInformation;
+    }
+}
---
+++ SiamCross/SiamCross/Models/Tools/MessageCreator.cs
@@ -0,0 +1,94 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SiamCross.Models.Tools
+{
+    public class MessageCreator
+    {
+        /// <summary>
+        /// Калькулятор контрольной суммы
+        /// </summary>
+        private CrcModbusCalculator _crcModbusCalculator =
+            new CrcModbusCalculator();
+
+        /// <summary>
+        /// Создать сообщение чтения
+        /// </summary>
+        /// <param name="address"></param>
+        /// <param name="dataSize"></param>
+        /// <returns></returns>
+        public byte[] CreateReadMessage(byte[] address, byte[] dataSize)
+        {
+            var bufList = new List<byte>() { 0x01, 0x01 };
+            bufList.AddRange(address);
+            for (int i = 0; i < 4 - address.Length; i++)
+            {
+                bufList.Add(0x00);
+            }
+
+            bufList.AddRange(dataSize);
+
+            var crc = _crcModbusCalculator.ModbusCrc(bufList.ToArray());
+
+            List<byte> result = new List<byte>() { 0x0D, 0x0A };
+            result.AddRange(bufList);
+            result.AddRange(crc);
+
+            return result.ToArray();
+        }
+
+        /// <summary>
+        /// Создать сообщение записи
+        /// </summary>
+        /// <param name="address"></param>
+        /// <param name="dataSize"></param>
+        /// <param name="data"></param>
+        /// <returns></returns>
+        public byte[] CreateWriteMessage(byte[] address, byte[] dataSize, byte[] data)
+        {
+            var addressAndSizeList = new List<byte>() { 0x01, 0x02 };
+            addressAndSizeList.AddRange(address);
+            for (int i = 0; i < 4 - address.Length; i++)
+            {
+                addressAndSizeList.Add(0x00);
+            }
+
+            addressAndSizeList.AddRange(dataSize);
+
+            var crcAS = _crcModbusCalculator.ModbusCrc(addressAndSizeList.ToArray());
+            var crcData = _crcModbusCalculator.ModbusCrc(data);
+
+            List<byte> result = new List<byte>() { 0x0D, 0x0A };
+            result.AddRange(addressAndSizeList);
+            result.AddRange(crcAS);
+            result.AddRange(data);
+            result.AddRange(crcData);
+
+            return result.ToArray();
+        }
+
+        /// <summary>
+        /// Создать сообщение записи
+        /// </summary>
+        /// <param name="deviceNameAddressAndSize"></param>
+        /// <param name="data"></param>
+        /// <returns></returns>
+        public byte[] CreateWriteMessage(byte[] deviceNameAddressAndSize, byte[] data)
+        {
+            var addressAndSizeList = new List<byte>() { 0x01, 0x02 };
+            addressAndSizeList.AddRange(deviceNameAddressAndSize);
+
+            var crcAS = _crcModbusCalculator.ModbusCrc(addressAndSizeList.ToArray());
+            var crcData = _crcModbusCalculator.ModbusCrc(data);
+
+            List<byte> result = new List<byte>() { 0x0D, 0x0A };
+            result.AddRange(addressAndSizeList);
+            result.AddRange(crcAS);
+            result.AddRange(data);
+            result.AddRange(crcData);
+
+            return result.ToArray();
+        }
+    }
+}
