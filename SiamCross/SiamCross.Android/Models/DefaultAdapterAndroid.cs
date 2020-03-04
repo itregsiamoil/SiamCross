45393eb8b6ce2054cfcb116b98961aee10e30d47
fox-dda <fox-dda@yandex.ru>
2020-03-04 11:34:25 +0700

    Добавлены файлы, котоорые были не в индексе

---
+++ SiamCross/SiamCross.Android/Models/DefaultAdapterAndroid.cs
@@ -0,0 +1,28 @@
+﻿using SiamCross.Models.Adapters;
+
+using Android.App;
+using Android.Content;
+using Android.OS;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+using Android.Bluetooth;
+
+
+namespace SiamCross.Droid.Models
+{
+    public class DefaultAdapterAndroid : IDefaultAdapter
+    {
+        public void Enable()
+        {
+            BluetoothAdapter.DefaultAdapter.Enable();
+        }
+
+        public void Disable()
+        {
+            BluetoothAdapter.DefaultAdapter.Disable();
+        }
+
+        public bool IsEnbaled { get => BluetoothAdapter.DefaultAdapter.IsEnabled; }
+    }
+}
---
+++ SiamCross/SiamCross/Models/Adapters/IDefaultAdapter.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SiamCross.Models.Adapters
+{
+    public interface IDefaultAdapter
+    {
+            void Disable();
+            void Enable();
+
+            bool IsEnbaled { get; }
+    }
+}
