76ff5fec11db77021f4d6212189701a6fda1ab9c
fox-dda <fox-dda@yandex.ru>
2020-03-05 14:47:05 +0700

    Добавлены комманды для DU

---
+++ SiamCross/SiamCross/Models/Sensors/Du/DuCommandsEnum.cs
@@ -0,0 +1,28 @@
+﻿namespace SiamCross.Models.Sensors.Du
+{
+    public enum DuCommandsEnum
+    {
+        DeviceType,
+        MemoryModelVersion,
+        DeviceNameAddress,
+        DeviceNameSize,
+        DeviceNumber,
+        ProgrammVersionAddress,
+        ProgrammVersionSize,
+        PressureSensitivity,
+        PiezoelectricSensitivity,
+        PressureZeroOffset,
+        PiezoelectricOffset,
+        Revbit,
+        Voltage,
+        Pressure,
+        StartMeasurement,
+        StartZeroingPressure,
+        Initialize,
+        StateZeroing,
+        SensorOff,
+        SensorState,
+        ResearchTitle,
+        ResearchData
+    }
+}
