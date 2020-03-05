2c1646cfbadcf4589e09a347a28d8b8b11f4e551
Гелцен Савындай <5842_sgy@keva.int>
2020-03-05 10:37:24 +0700

    Добавлен класс сенсора ДУ

---
+++ SiamCross/SiamCross/Models/Sensors/Du/DuSensor.cs
@@ -0,0 +1,39 @@
+﻿using SiamCross.Models.Scanners;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SiamCross.Models.Sensors.Du
+{
+    public class DuSensor : ISensor
+    {
+        public IBluetoothAdapter BluetoothAdapter => throw new NotImplementedException();
+
+        public bool IsAlive => throw new NotImplementedException();
+
+        public bool IsMeasurement => throw new NotImplementedException();
+
+        public SensorData SensorData => throw new NotImplementedException();
+
+        public ScannedDeviceInfo ScannedDeviceInfo { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
+
+        public event Action<SensorData> Notify;
+
+        public void Dispose()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task QuickReport()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task StartMeasurement(object measurementParameters)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
