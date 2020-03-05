eef619b9b4b8ad031887ba3620766a12e5af9e39
Гелцен Савындай <5842_sgy@keva.int>
2020-03-05 11:14:41 +0700

    Добавлены папки для ДУ

--- SiamCross/SiamCross/Models/Sensors/Du/DuSensor.cs
+++ SiamCross/SiamCross/Models/Sensors/Du/DuSensor.cs
@@ -3,27 +3,51 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 
 namespace SiamCross.Models.Sensors.Du
 {
     public class DuSensor : ISensor
     {
-        public IBluetoothAdapter BluetoothAdapter => throw new NotImplementedException();
+        public IBluetoothAdapter BluetoothAdapter { get; }
+        public ScannedDeviceInfo ScannedDeviceInfo { get; set; }
 
-        public bool IsAlive => throw new NotImplementedException();
-
-        public bool IsMeasurement => throw new NotImplementedException();
-
-        public SensorData SensorData => throw new NotImplementedException();
-
-        public ScannedDeviceInfo ScannedDeviceInfo { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
 
         public event Action<SensorData> Notify;
 
+        public SensorData SensorData { get; }
+        public bool IsMeasurement { get; }
+        public bool IsAlive { get; }
+        private Task _liveTask;
+        private CancellationTokenSource _cancelSource =
+            new CancellationTokenSource();
+
+        public DuSensor(IBluetoothAdapter adapter, 
+                        SensorData sensorData)
+        {
+            BluetoothAdapter = adapter;
+            SensorData = sensorData;
+
+            IsMeasurement = false;
+            IsAlive = false;
+            _liveTask = new Task(async () => await LiveWhile(_cancelSource.Token));
+        }
+
+        private async Task LiveWhile(CancellationToken token)
+        {
+            while (!token.IsCancellationRequested)
+            {
+                if (IsAlive)
+                {
+
+                }
+            }
+        }
+
         public void Dispose()
         {
-            throw new NotImplementedException();
+            
         }
 
         public Task QuickReport()
