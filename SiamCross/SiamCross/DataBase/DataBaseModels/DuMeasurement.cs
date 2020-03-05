60fe93f8fb571caecf9b1e92636de253637ebbd0
Гелцен Савындай <5842_sgy@keva.int>
2020-03-05 11:48:30 +0700

    Добавлена модель данных измерения ДУ

---
+++ SiamCross/SiamCross/DataBase/DataBaseModels/DuMeasurement.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SiamCross.DataBase.DataBaseModels
+{
+    public class DuMeasurement
+    {
+        public int Id { get; set; }
+
+        public UInt16 Urov { get; set; }
+
+        public UInt16 Otr { get; set; }
+
+        public Byte[] Echogram { get; set; }
+    }
+}
