b0a2e1ee2ac11a92e46d46c3522eb382aa62e145
Гелцен Савындай <5842_sgy@keva.int>
2020-03-11 09:41:35 +0700

    добавлена вьюмоделька Du

---
+++ SiamCross/SiamCross/ViewModels/DuMeasurementViewModel.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Text;
+
+namespace SiamCross.ViewModels
+{
+    public class DuMeasurementViewModel: BaseViewModel, IViewModel
+    {
+        public ObservableCollection<string> Fields { get; set; }
+        public string SelectedField { get; set; }
+        public string Well { get; set; }
+        public string Bush { get; set; }
+        public string Shop { get; set; }
+        public string BufferPressure { get; set; }
+        public string Comments { get; set; }
+        public string DynPeriod { get; set; }
+        public string ApertNumber { get; set; }
+        public string Imtravel { get; set; }
+    }
+}
