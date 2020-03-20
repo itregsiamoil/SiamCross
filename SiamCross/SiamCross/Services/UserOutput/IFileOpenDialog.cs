fa9dea074035cd799a4b1b744c4e9d5df7c6535a
fox-dda <fox-dda@yandex.ru>
2020-03-20 14:10:12 +0700

    Добавлена реализация на андроиде визуализатора выбора файла

---
+++ SiamCross/SiamCross/Services/UserOutput/IFileOpenDialog.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SiamCross.Services.UserOutput
+{
+    public interface IFileOpenDialog
+    {
+        Task<string> Show();
+    }
+}
