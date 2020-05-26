5a441b8fd48635b6271a24dc820ce8f38a2ddc39
Dmitry Domaskin <fox-dda@yandex.ru>
2020-05-26 21:12:29 +0700

    Реализован класс представление тиблицы устройств в кастомном модеме

---
+++ SiamCross/SiamCross/Models/USB/HardwareDevicesTable.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Concurrent;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SiamCross.Models.USB
+{
+    public class HardwareDevicesTable
+    {
+        private ConcurrentBag<HardwareDeviceTableItem> _itemBag;
+
+        public HardwareDevicesTable()
+        {
+            _itemBag = new ConcurrentBag<HardwareDeviceTableItem>();
+        }
+
+        public void Clear()
+        {
+            HardwareDeviceTableItem someItem;
+            while (!_itemBag.IsEmpty)
+            {
+                _itemBag.TryTake(out someItem);
+            }
+        }
+
+        public void AddOrRefresh(int number, string address)
+        {
+            foreach (var item in _itemBag)
+            {
+                if (item.Number == number)
+                {
+                    item.Address = address;
+                    return;
+                }
+            }
+
+            _itemBag.Add(new HardwareDeviceTableItem(number, address));
+        }
+
+        public string GetAddressForNumber(int number)
+        {
+            foreach (var item in _itemBag)
+            {
+                if (item.Number == number)
+                {
+                    return item.Address;
+                }
+            }
+
+            return null;
+        }
+
+        public int GetNumberForAddress(string address)
+        {
+            foreach (var item in _itemBag)
+            {
+                if (item.Address == address)
+                {
+                    return item.Number;
+                }
+            }
+
+            return -1;
+        }
+    }
+
+    internal class HardwareDeviceTableItem
+    {
+        public int Number { get; set; }
+        public string Address { get; set; }
+
+        public HardwareDeviceTableItem(int number, string address)
+        {
+            Number = number;
+            Address = address;
+        }
+    }
+}
