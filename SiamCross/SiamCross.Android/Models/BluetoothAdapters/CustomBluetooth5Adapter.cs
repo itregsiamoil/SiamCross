70f960abb0c43fe4649f3aad13d7eb1b3369afc4
Dmitry Domaskin <fox-dda@yandex.ru>
2020-05-27 22:16:05 +0700

    Реализованы методы адаптера

--- SiamCross/SiamCross.Android/Models/BluetoothAdapters/CustomBluetooth5Adapter.cs
+++ SiamCross/SiamCross.Android/Models/BluetoothAdapters/CustomBluetooth5Adapter.cs
@@ -11,25 +11,66 @@ namespace SiamCross.Droid.Models.BluetoothAdapters
     public class CustomBluetooth5Adapter : IBluetoothAdapter, IUsbDataObserver
     {
         public string Address { get; private set; }
+        private bool _isConnected;
+        private USBService _usbService;
+        private int _forceScanCounter;
 
         public CustomBluetooth5Adapter(string address)
         {
             Address = address;
+            _isConnected = false;
+            _usbService = USBService.Instance;
+            _forceScanCounter = 0;
+
+            _usbService.RegisterUsbObserver(this as IUsbDataObserver);
         }
 
-        public Task Connect()
+        public async Task Connect()
         {
-            throw new NotImplementedException();
+            try
+            {
+                _usbService.ConnectQuery(Address);
+            }
+            catch (NotSupportedException exception)
+            {
+                Console.WriteLine($@"Connect metod error {Address} : {exception.Message}; force scan counter = {_forceScanCounter}");
+                _forceScanCounter++;
+                if (_forceScanCounter % 5 == 1)
+                {
+                    Console.WriteLine($@"{Address} adapter forced scan!");
+                    _usbService.StartScanQuery();
+                    return;
+                }
+            }
+
+            await Task.Delay(5000);
+
+            if (_isConnected)
+            {
+                _forceScanCounter = 0;
+                ConnectSucceed?.Invoke();
+            }
+            else
+            {
+                throw new TimeoutException("Connection timed out!");
+            }
         }
 
-        public Task Disconnect()
+        public async Task Disconnect()
         {
-            throw new NotImplementedException();
+            _usbService.DisconnecDeviceQuery(Address);
         }
 
-        public Task SendData(byte[] data)
+        public async Task SendData(byte[] data)
         {
-            throw new NotImplementedException();
+            try
+            {
+                _usbService.SendDataQuery(data, Address);
+            }
+            catch (NotSupportedException exception)
+            {
+                ConnectFailed?.Invoke();
+            }
         }
 
         public void OnDataRecieved(byte[] data)
@@ -39,12 +80,32 @@ namespace SiamCross.Droid.Models.BluetoothAdapters
 
         public void OnConnectSucceed()
         {
-            ConnectSucceed?.Invoke();
+            _isConnected = true;
         }
 
         public void OnDisconnected()
         {
+            _isConnected = false;
             ConnectFailed?.Invoke();
+        }
+
+        public override bool Equals(Object obj)
+        {
+            if ((obj == null) || this.GetType() != obj.GetType())
+            {
+                return false;
+            }
+            else
+            {
+                if (((CustomBluetooth5Adapter) obj).Address.Equals(Address))
+                {
+                    return true;
+                }
+                else
+                {
+                    return false;
+                }
+            }
         }
 
         public event Action<byte[]> DataReceived;
