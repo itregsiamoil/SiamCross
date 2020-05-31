0446c8bf75e33654dc20753e35a41a7e2cd49e3f
Dmitry Domaskin <fox-dda@yandex.ru>
2020-05-31 23:56:04 +0700

    Изменен конструктор

--- SiamCross/SiamCross.Android/Models/BluetoothAdapters/CustomBluetooth5Adapter.cs
+++ SiamCross/SiamCross.Android/Models/BluetoothAdapters/CustomBluetooth5Adapter.cs
@@ -4,20 +4,22 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using SiamCross.Models;
+using SiamCross.Models.Adapters;
+using SiamCross.Models.Scanners;
 using SiamCross.Models.USB;
 
 namespace SiamCross.Droid.Models.BluetoothAdapters
 {
-    public class CustomBluetooth5Adapter : IBluetoothAdapter, IUsbDataObserver
+    public class CustomBluetooth5Adapter : IBluetooth5CustomAdapter, IUsbDataObserver
     {
         public string Address { get; private set; }
         private bool _isConnected;
         private USBService _usbService;
-        private int _forceScanCounter;
+        private static int _forceScanCounter;
 
-        public CustomBluetooth5Adapter(string address)
+        public CustomBluetooth5Adapter(ScannedDeviceInfo deviceInfo)
         {
-            Address = address;
+            Address = deviceInfo.BluetoothArgs as string;
             _isConnected = false;
             _usbService = USBService.Instance;
             _forceScanCounter = 0;
@@ -35,7 +37,7 @@ namespace SiamCross.Droid.Models.BluetoothAdapters
             {
                 Console.WriteLine($@"Connect metod error {Address} : {exception.Message}; force scan counter = {_forceScanCounter}");
                 _forceScanCounter++;
-                if (_forceScanCounter % 5 == 1)
+                if (_forceScanCounter % 15 == 1)
                 {
                     Console.WriteLine($@"{Address} adapter forced scan!");
                     _usbService.StartScanQuery();
@@ -45,15 +47,15 @@ namespace SiamCross.Droid.Models.BluetoothAdapters
 
             await Task.Delay(5000);
 
-            if (_isConnected)
-            {
-                _forceScanCounter = 0;
-                ConnectSucceed?.Invoke();
-            }
-            else
-            {
-                throw new TimeoutException("Connection timed out!");
-            }
+            //if (_isConnected)
+            //{
+            //    _forceScanCounter = 0;
+            //    ConnectSucceed?.Invoke();
+            //}
+            //else
+            //{
+            //    //throw new TimeoutException("Connection timed out!");
+            //}
         }
 
         public async Task Disconnect()
@@ -81,6 +83,8 @@ namespace SiamCross.Droid.Models.BluetoothAdapters
         public void OnConnectSucceed()
         {
             _isConnected = true;
+            _forceScanCounter = 0;
+            ConnectSucceed?.Invoke();
         }
 
         public void OnDisconnected()
