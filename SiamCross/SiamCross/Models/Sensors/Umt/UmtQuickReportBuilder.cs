7b5964f04c53082b1a0dea8eebea6988d98bab20
fox-dda <fox-dda@yandex.ru>
2020-06-02 15:12:15 +0700

    Реализован билдер быстрого замера для УМТ

---
+++ SiamCross/SiamCross/Models/Sensors/Umt/UmtQuickReportBuilder.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SiamCross.Models.Sensors.Umt
+{
+    public class UmtQuickReportBuilder
+    {
+        private string _batteryVoltage;
+        private string _pressure;
+
+
+        public string BatteryVoltage
+        {
+            get
+            {
+                return
+                    _batteryVoltage != null ?
+                        $"{Resource.Voltage}: "
+                        + _batteryVoltage
+                        + $", {Resource.VoltsUnits}\n"
+                    : "";
+            }
+
+            set
+            {
+                _batteryVoltage = value;
+            }
+        }
+
+        public string Pressure
+        {
+            get
+            {
+                return _pressure != null ?
+                    $"{Resource.Pressure}: "
+                    + _pressure
+                    + $", {Resource.KGFCMUnits}\n"
+                : "";
+            }
+
+            set
+            {
+                _pressure = value;
+            }
+        }
+
+        public string GetReport()
+        {
+            return BatteryVoltage + Pressure;
+        }
+    }
+}
