96d0ea7ee8146674efada4b19e426a37c3bece50
fox-dda <fox-dda@yandex.ru>
2020-06-17 11:55:37 +0700

    Добавлены в индекс недостающие файлы

---
+++ SiamCross/SiamCross/Models/Sensors/Umt/Measurement/UmtMeasurementStatus.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SiamCross.Models.Sensors.Umt.Measurement
+{
+    public enum UmtMeasurementStatus
+    {
+        Empty = 0,
+        Measurement
+    }
+}
