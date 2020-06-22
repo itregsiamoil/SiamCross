f08e96a05acbec7a007c78b0d19c9e9ab39d29f7
fox-dda <fox-dda@yandex.ru>
2020-06-22 11:54:28 +0700

    testtest

---
+++ SiamCross/SiamCross/Models/Sensors/Umt/UmtCommandsEnum.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SiamCross.Models.Sensors.Umt
+{
+    public enum UmtCommandsEnum
+    {
+        //Общие регистры
+        DeviceType,
+        MemoryModelVersion,
+        DeviceNameAddress,
+        DeviceNameSize,
+        DeviceNumber,
+        Adrtec,
+        Ukstr,
+        Ukbl,
+        Page,
+        Kolstr,
+        Kolbl,
+        BaundRate,
+        //Справочные данные
+        ProgrammVersionAddress,
+        ProgrammVersionSize,
+        //Таблица валидных блоков
+        Tabinv,
+        //Параметры прибора и исследования
+        Vissl,
+        Kust,
+        Skv,
+        Field,
+        Shop,
+        Operator,
+        Noldav,
+        Interval,
+        Revbit,
+        Cher,
+        //Текущие данные
+        Dav,
+        Temp,
+        ExTemp,
+        Acc,
+        RaznR,
+        ObshR,
+        Emak,
+        Emem,
+        Kolisl,
+        Schstr,
+        Schbl,
+        Koliz,
+        Time,
+        TIMEost,
+        //Операционные регистры
+        StartMeasurement,
+        StartNullatingPressureSensor,
+        Initialize,
+        StartNullatingDevice,
+        OffDevice,
+        Sostdat,
+        //Прочее
+        MeasurePage,
+        ResearchHeaders,
+        DeviceProgrammVersion,
+        CurrentParametersFrame,
+        StubCommand
+    }
+}
---
+++ SiamCross/SiamCross/Models/Sensors/Umt/UmtQuickReportBuilder.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SiamCross.Models.Sensors.Umt
+{
+    public class UmtQuickReportBuilder
+    {
+        private string _batteryVoltage;
+        private string _pressure;
+        private string _temperature;
+
+        public string Temperature
+        {
+            get
+            {
+                return
+                    _temperature != null ?
+                        $"{Resource.Temperature}: "
+                        + _temperature
+                        + $", {Resource.DegCentigradeUnits}\n"
+                    : "";
+            }
+
+            set
+            {
+                _temperature = value;
+            }
+        }
+
+        public string BatteryVoltage
+        {
+            get
+            {
+                return
+                    _batteryVoltage != null ?
+                        $"{Resource.Voltage}: "
+                        + _batteryVoltage
+                        + $", {Resource.VoltsUnits}\n"
+                    : "";
+            }
+
+            set
+            {
+                _batteryVoltage = value;
+            }
+        }
+
+        public string Pressure
+        {
+            get
+            {
+                return _pressure != null ?
+                    $"{Resource.Pressure}: "
+                    + _pressure
+                    + $", {Resource.AtmosphereUnits}\n"
+                : "";
+            }
+
+            set
+            {
+                _pressure = value;
+            }
+        }
+
+        public string GetReport()
+        {
+            return BatteryVoltage + Temperature + Pressure;
+        }
+    }
+}
