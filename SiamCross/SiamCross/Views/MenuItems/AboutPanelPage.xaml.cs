4ab9c4431ce2c11ad08b7874107904ddc3e5d255
DESKTOP-L40LF59\BuildServer <ASavinov@integra.ru>
2020-12-14 15:44:51 +0700

    fix #35 Добавить отображение информации об изменении в меню о программе

--- SiamCross/SiamCross/Views/MenuItems/AboutPanelPage.xaml.cs
+++ SiamCross/SiamCross/Views/MenuItems/AboutPanelPage.xaml.cs
@@ -1,7 +1,9 @@
 ﻿using SiamCross.Models.Tools;
 using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
+using System.Reflection;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -32,6 +34,35 @@ namespace SiamCross.Views.MenuItems
             get
             {
                 return DependencyService.Get<IAppVersionAndBuild>().GetBuildNumber();
+            }
+        }
+
+        public string VersionChanges
+        {
+            get
+            {
+            //Получаем текущую сборку.
+                var myAssembly = Assembly.GetExecutingAssembly();
+                string resourceName = "VersionChanges.txt"; // В этом поле указываем имя нашего ресурса.
+                                                   //(!) Если ресурс находится в папке, то указываем полный путь к нему, например "content\resource\image.jpg". Но не забываем, что при формировании полного имени ресурса, заменяем знак cлэш '\' на точку '.'
+                //Формируем полное имя ресурса.
+                string fullResourceName = $"{myAssembly.GetName().Name}.{resourceName.Replace('\\', '.')}";
+                bool isExistsResourceName = myAssembly.GetManifestResourceNames()
+                    .Contains(fullResourceName); //уточняем существует этот ресурс в данной сборке.
+
+                
+                string ret="";
+                byte[] result;
+                StringBuilder builder = new StringBuilder();
+                //Если ресурс существует, то извлекаем его.
+                if (isExistsResourceName)
+                {
+                    Stream stream = myAssembly.GetManifestResourceStream(fullResourceName);
+                    result = new byte[stream.Length];
+                    var reader = new StreamReader(stream);
+                    ret = reader.ReadToEnd();
+                }
+                return ret;
             }
         }
 
