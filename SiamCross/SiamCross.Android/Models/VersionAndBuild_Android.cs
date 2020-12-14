17a601ed8f41612c143b17c8042788188b2527e2
DESKTOP-L40LF59\BuildServer <ASavinov@integra.ru>
2020-12-14 12:44:10 +0700

    resolve #13  APP - Отображение версии приложения из манифеста - переделать на Xamarin.Essentials

--- SiamCross/SiamCross.Android/Models/VersionAndBuild_Android.cs
+++ SiamCross/SiamCross.Android/Models/VersionAndBuild_Android.cs
@@ -1,5 +1,6 @@
 ﻿using Android.Content.PM;  
-using VersionAndBuildNumber.Droid.DependencyServices;  
+using VersionAndBuildNumber.Droid.DependencyServices;
+using Xamarin.Essentials;
 using Xamarin.Forms;  
 
 [assembly: Dependency(typeof(VersionAndBuild_Android))]  
@@ -7,19 +8,19 @@ namespace VersionAndBuildNumber.Droid.DependencyServices
 {
     public class VersionAndBuild_Android : SiamCross.Models.Tools.IAppVersionAndBuild
     {
-        PackageInfo _appInfo;
+        //PackageInfo _appInfo;
         public VersionAndBuild_Android()
         {
-            var context = Android.App.Application.Context;
-            _appInfo = context.PackageManager.GetPackageInfo(context.PackageName, 0);
+            //var context = Android.App.Application.Context;
+            //_appInfo = context.PackageManager.GetPackageInfo(context.PackageName, 0);
         }
         public string GetVersionNumber()
         {
-            return _appInfo.VersionName;
+            return AppInfo.VersionString;
         }
         public string GetBuildNumber()
         {
-            return _appInfo.VersionCode.ToString();
+            return AppInfo.BuildString;
         }
     }
 }
