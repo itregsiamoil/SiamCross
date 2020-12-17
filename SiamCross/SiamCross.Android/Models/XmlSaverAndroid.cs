931a40b639a4da07476e8c060afa07ab2819bbb5
DESKTOP-L40LF59\BuildServer <ASavinov@integra.ru>
2020-12-17 12:50:00 +0700

    fix #41 Исправить отправку логику сохранения измерений

--- SiamCross/SiamCross.Android/Models/XmlSaverAndroid.cs
+++ SiamCross/SiamCross.Android/Models/XmlSaverAndroid.cs
@@ -1,18 +1,10 @@
-﻿using System;
-using System.Collections.Generic;
-using System.IO;
-using System.Linq;
-using System.Text;
-using System.Xml.Linq;
-using Android.App;
-using Android.Content;
-using Android.Media;
-using Android.OS;
-using Android.Runtime;
-using Android.Views;
-using Android.Widget;
-using SiamCross.Droid.Models;
+﻿using SiamCross.Droid.Models;
 using SiamCross.Models.Tools;
+using System;
+using System.IO;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Xml.Linq;
 using Xamarin.Forms;
 
 [assembly: Dependency(typeof(XmlSaverAndroid))]
@@ -25,48 +17,63 @@ namespace SiamCross.Droid.Models
 
         private readonly string _folder = "Measurements";
 
-        private readonly object _locker = new object();
-
         public void DeleteXml(string filename)
         {
-            lock (_locker)
-            {
-                string s = Directory.CreateDirectory(_path +
-                    (Path.DirectorySeparatorChar + _folder)).FullName;
+            string s = Directory.CreateDirectory(_path +
+                (Path.DirectorySeparatorChar + _folder)).FullName;
 
-                if (Directory.Exists(s))
+            if (Directory.Exists(s))
+            {
+                var filepath = s + Path.DirectorySeparatorChar + filename;
+                if (File.Exists(filepath))
                 {
-                    var filepath = s + Path.DirectorySeparatorChar + filename;
-                    if (File.Exists(filepath))
-                    {
-                        File.Delete(filepath);
-                    }
+                    File.Delete(filepath);
                 }
             }
         }
-
-        public void SaveXml(string filename, XDocument xml)
+        public async Task<bool> SaveXml(string filename, XDocument xml)
         {
             //string s = Directory.CreateDirectory(_path + 
             //    (Path.DirectorySeparatorChar + _folder)).FullName;
-            lock (_locker)
-            {
-                string s = Directory.CreateDirectory(@"/storage/emulated/0/" + (Path.DirectorySeparatorChar + _folder)).FullName;
+            string s = Directory.CreateDirectory(@"/storage/emulated/0/" + (Path.DirectorySeparatorChar + _folder)).FullName;
+            if (!Directory.Exists(s))
+                return false;
 
-
-                if (Directory.Exists(s))
-                {
-                    var fullPath = s + (Path.DirectorySeparatorChar + filename);
-                    //if (!File.Exists(fullPath))
-                    {
-                        xml.Save(fullPath);
-                        MediaScannerConnection.ScanFile(Android.App.Application.Context, new String[] { fullPath }, null, null);
-                    }
-                }
-
-            }
+            var fullPath = s + (Path.DirectorySeparatorChar + filename);
+            FileStream fs = TryCreateFileStream(fullPath);
+            if(null==fs)
+                return false;
+            //File.SetAttributes(fullPath, FileAttributes.Normal);
+            //FileIOPermission filePermission =new FileIOPermission(FileIOPermissionAccess.AllAccess, fullPath);
+            System.Xml.XmlWriterSettings settings = new System.Xml.XmlWriterSettings();
+            settings.Async = true;
+            System.Xml.XmlWriter writer = System.Xml.XmlWriter.Create(fs, settings);
+            CancellationTokenSource cts = new CancellationTokenSource(3000);
+            await xml.SaveAsync(writer, cts.Token);
+            //MediaScannerConnection.ScanFile(Android.App.Application.Context, new String[] { fullPath }, null, null);
+            await fs.FlushAsync();
+            writer?.Dispose();
+            await fs.DisposeAsync();
+            cts?.Dispose();
+            return true;
         }
-
+        FileStream TryCreateFileStream(string filename)
+        {
+            FileStream fs = null;
+            try
+            {
+                fs = new FileStream(filename, FileMode.Create, FileAccess.Write, FileShare.None);
+            }
+            catch (Exception)
+            {
+                if (File.Exists(filename))
+                {
+                    File.Delete(filename);
+                    fs = new FileStream(filename, FileMode.Create, FileAccess.Write, FileShare.None);
+                }
+            }
+            return fs;
+        }
         /// <summary>
         /// Получить путь к файлу
         /// </summary>
--- SiamCross/SiamCross/Models/Tools/IXmlSaver.cs
+++ SiamCross/SiamCross/Models/Tools/IXmlSaver.cs
@@ -1,10 +1,11 @@
-﻿using System.Xml.Linq;
+﻿using System.Threading.Tasks;
+using System.Xml.Linq;
 
 namespace SiamCross.Models.Tools
 {
     public interface IXmlSaver
     {
-        void SaveXml(string filename, XDocument xml);
+        Task<bool> SaveXml(string filename, XDocument xml);
         void DeleteXml(string filename);
 
         string GetFilepath(string filename);
